25844e307e0fc3f99174aee76371c426252c59e3
alepnabil <aliffni72@gmail.com>
2021-12-27 14:34:09 +0800

    scrape users review

--- english/politics/scrape.py
+++ english/politics/scrape.py
@@ -41,16 +41,44 @@ class Reviews(Scrape):
     def get_usernames(self):
         time.sleep(2)
         username=self.soup.find_all('a',class_='user')
+        Username=[user.text for user in username]
         print(f'----FOUND {len(username)} USERS REVIEWS-----')
+
+        #check all the user's username
         for user in username:
             print(user.text)
+        return Username
+        
 
     def get_reviews(self):
+        final_review=[]
         time.sleep(2)
-        print('asdasda')
-        reviews=self.soup.find_all('')   
+        #doesnt get all the reviews(the short ones), cannot get the short reviews since the short reviews is not under span.readbale
+        #reviews_body=self.soup.select('div#reviews span.readable [style="display:none"]')
+        #navigate to review body
+        reviews_body=self.soup.find_all('div',class_='reviewText stacked')
+        #navigate to each review body
+        for review in reviews_body:
+            #in each body there will be 2 types of reviews (shorter and longer)
+            #so we want to take the longer version
+            words=review.find_all('span')
+            #get the clean text
+            clean_review=[review.text for review in words]
+            #split both short and long reviews and get the longest review for each users
+            temp_review=clean_review[0].split('\n',2)
+            longest_review=(max(temp_review,key=len))
+            final_review.append(longest_review)
+            print(longest_review)
+            print('-------------------------------------------------------------------')
+
+        #supposedly there should be 30 reviews
+        print(f'---------------FOUND {len(final_review)} REVIEWS---------------')
+        return final_review
+            
+            
 
 main_page=Reviews(ChromeDriverManager().install(),'https://www.goodreads.com/book/show/19083.Politics')
 #call the main function to get request from main page, if we dont call the main page requester our soup object wont be pass
 main_page.get_request()
 main_page.get_usernames()
+main_page.get_reviews()

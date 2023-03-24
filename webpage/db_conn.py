0c94733324ba9fc3fc07126a233394101b789a71
alepnabil <aliffni72@gmail.com>
2023-03-24 23:09:32 +0800

    update db connection

--- webpage/db_conn.py
+++ webpage/db_conn.py
@@ -1,15 +1,10 @@
-import mysql.connector
 import streamlit as st
+import mysql.connector
 
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
 
-host=st.secrets["host"]
-user=st.secrets["user"]
-password=st.secrets["password"]
-database=st.secrets["database"]
-
-mydb = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-        )
+mydb = init_connection()
